c7b9c8e269b2013498df096e7781e51723cac253
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-05-18 02:45:37 -0300

    inicializacao do projeto

---
+++ api-GeneratorCurriculum/src/main/java/com/apiGeneratorCurriculum/ApiGeneratorCurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.apiGeneratorCurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeneratorCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeneratorCurriculumApplication.class, args);
+	}
+
+}
---
+++ api-GeneratorCurriculum/src/test/java/com/apiGeneratorCurriculum/ApiGeneratorCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiGeneratorCurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeneratorCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
