5c16d654d3aac94f9d3765a543ef574e7d93e4a4
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-05-21 15:21:07 -0300

    aplicando estrutura front-end

--- ui-GeneratorCurriculum/src/app/app.component.ts
+++ ui-GeneratorCurriculum/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui-GeneratorCurriculum';
+
+  nome!: string;
+  cargo_atual!: string;
+  estado!: string;
+  pais!: string;
+  numero_contato!: string;
+  email_contato!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
+
+
